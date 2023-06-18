066c7a45f1152150b20e270084ad31c875f09939
LeoraChriquiRestart <127514989+LeoraChriquiRestart@users.noreply.github.com>
2023-06-18 14:07:26 +0200

    Add files via upload

--- projet_axe/template/hoofbeatnav.php
+++ projet_axe/template/hoofbeatnav.php
@@ -1,64 +1,67 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <nav class="left-box" id="sidenav">     
-        <div class="options accueil">
-            <span class="material-symbols-outlined">home</span>
-            <h2>
-                <a href="index.php">Accueil</a>
-            </h2>        
-        </div>
-                
-        <div class="options">
-            <span class="material-symbols-outlined">login</span>               
-            <h2>
-                <a href="connexion.php">Connexion</a>
-            </h2>
-        </div>
+<nav class="left-box" id="sidenav">     
+    <div class="options accueil">
+        <span class="material-symbols-outlined">home</span>
+        <h2>
+            <a href="index.php">Accueil</a>
+        </h2>        
+    </div>
+            
+    <div class="options">
+        <span class="material-symbols-outlined">login</span>               
+        <h2>
+            <a href="connexion.php">Connexion</a>
+        </h2>
+    </div>
 
-        <div class="options">
-            <span class="material-symbols-outlined">more_horiz</span>
-            <h2>
-                <a href="deconnexion.php">Déconnexion</a>
-            </h2>
-        </div>
-                
-        <div class="options">
-            <span class="material-symbols-outlined">stylus</span>
-            <h2>
-                <a href="inscription.php">Inscription</a>
-            </h2>
-        </div>
+    <div class="options">
+    <span class="material-symbols-outlined">logout</span>
+        <h2>
+            <a href="deconnexion.php">Déconnexion</a>
+        </h2>
+    </div>
+            
+    <div class="options">
+        <span class="material-symbols-outlined">stylus</span>
+        <h2>
+            <a href="inscription.php">Inscription</a>
+        </h2>
+    </div>
+
+    <div class="options">
+        <span class="material-symbols-outlined">more_horiz</span>
+        <h2>
+            <a href="plus.php">Plus</a>
+        </h2>
+    </div>
     
-        <div class="options">
-            <span class="material-symbols-outlined">more_horiz</span>
-            <h2>
-                <a href="plus.php">Plus</a>
-            </h2>
-        </div>
-        
+
+    <button class="post-box__post-button" id="mon-bouton">Post</button>
     
-        <button class="post-box__post-button" id="mon-bouton">Post</button>
-        
-        <div class="modal">
-            <div class="modal-content">
+    <div class="modal">
+        <div class="modal-content">
+            <form class="form" method="POST" action="post.php">
                 <span class="close">&times;</span>
                 <div class="post-box__input">
                     <img src="https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png" alt="avatar">
-                    <input type="text" placeholder="Quoi de neuf ?">
-                    <button class="post-box__post-button">Post</button>
+                    <input type="text" name="contenu" placeholder="Quoi de neuf ?">
+                    <select name="tag" required>
+                        <option value="">#</option>
+                        <option id="tag1">#Ethologie</option>
+                        <option id="tag2">#Bien être</option>
+                        <option id="tag3">#Saut</option>
+                        <option id="tag4">#Random</option>
+                        <option id="tag5">#Dressage</option>
+                        <option id="tag6">#Club</option>
+                        <option id="tag7">#Propriétaire</option>
+                        <option id="tag8">#Compétition</option>
+                        <option id="tag9">#TAP</option>
+                        <option id="tag10">#Loisirs</option>
+                    </select>
                 </div>
-            </div>
+                <button class="post-box__post-button">Post</button>
+            </form>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    <script src="script.js"></script> 
-</body>
-</html>
+                
