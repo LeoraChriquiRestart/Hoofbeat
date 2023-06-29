ad9e17b887a46e4305db71fc525fb0314c5ae117
LeoraChriquiRestart <127514989+LeoraChriquiRestart@users.noreply.github.com>
2023-06-30 01:02:57 +0200

    Add files via upload

--- projet_axe/template/hoofbeatnav.php
+++ projet_axe/template/hoofbeatnav.php
@@ -12,13 +12,14 @@
             <a href="connexion.php">Connexion</a>
         </h2>
     </div>
-
-    <div class="options">
-    <span class="material-symbols-outlined">logout</span>
-        <h2>
-            <a href="deconnexion.php">Déconnexion</a>
-        </h2>
-    </div>
+    <?php if (isset($_SESSION['id'])) { ?>
+        <div class="options">
+            <span class="material-symbols-outlined">logout</span>
+            <h2>
+                <a href="deconnexion.php">Déconnexion</a>
+            </h2>
+        </div>
+    <?php } ?>
             
     <div class="options">
         <span class="material-symbols-outlined">stylus</span>
@@ -27,24 +28,26 @@
         </h2>
     </div>
 
-    <div class="options">
-        <span class="material-symbols-outlined">person</span>
-        <h2>
-            <a href="profil.php">Profil</a>
-        </h2>
-    </div>
+    <?php if (isset($_SESSION['id'])) { ?>
+        <div class="options">
+            <span class="material-symbols-outlined">person</span>
+            <h2>
+                <a href="profil.php">Profil</a>
+            </h2>                    
+        </div>
+    <?php } ?>
     
 
     <button class="post-box__post-button" id="mon-bouton">Post</button>
     
     <div class="modal">
         <div class="modal-content">
-            <form class="form" method="POST" action="post.php">
+            <form class="form" method="POST" action="post.php" enctype="multipart/form-data">
                 <span class="close">&times;</span>
                 <div class="post-box__input">
                     <img src="https://media.idownloadblog.com/wp-content/uploads/2017/03/Twitter-new-2017-avatar-001.png" alt="avatar">
-                    <input type="text" name="contenu" placeholder="Quoi de neuf ?">
-                    <select name="tag" required>
+                    <input type="text" name="contenu" placeholder="Quoi de neuf ?" id="contenu">
+                    <select name="tag" id="tag" required>
                         <option value="">#</option>
                         <option id="tag1">#Ethologie</option>
                         <option id="tag2">#Bien être</option>
@@ -57,6 +60,9 @@
                         <option id="tag9">#TAP</option>
                         <option id="tag10">#Loisirs</option>
                     </select>
+                </div>
+                <div class="post-box__images">
+                    <input type="file" accept="image/jpg, image/png, image/gif" name="images">
                 </div>
                 <button class="post-box__post-button">Post</button>
             </form>
